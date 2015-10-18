38a093675200d0cb6cb4a7ecb0d64a7194d4db11
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-10-17 17:20:13 -0700

    Removed some scaffold code and revised the strong parameters for maps and datasets

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -24,41 +24,22 @@ class DatasetsController < ApplicationController
   # POST /datasets
   # POST /datasets.json
   def create
-    @dataset = Dataset.new(dataset_params)
 
-    respond_to do |format|
-      if @dataset.save
-        format.html { redirect_to @dataset, notice: 'Dataset was successfully created.' }
-        format.json { render :show, status: :created, location: @dataset }
-      else
-        format.html { render :new }
-        format.json { render json: @dataset.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    # Validate params
+    # Save file somewhere
+    # Read columns
+    # Create dataset creation params
+    # Create dataset
+    # Create columns
+    # Send responses
 
-  # PATCH/PUT /datasets/1
-  # PATCH/PUT /datasets/1.json
-  def update
-    respond_to do |format|
-      if @dataset.update(dataset_params)
-        format.html { redirect_to @dataset, notice: 'Dataset was successfully updated.' }
-        format.json { render :show, status: :ok, location: @dataset }
-      else
-        format.html { render :edit }
-        format.json { render json: @dataset.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /datasets/1
   # DELETE /datasets/1.json
   def destroy
-    @dataset.destroy
-    respond_to do |format|
-      format.html { redirect_to datasets_url, notice: 'Dataset was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # Delete the dataset file first
+    # Delete the datafile
   end
 
   private
@@ -69,6 +50,7 @@ class DatasetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dataset_params
-      params[:dataset]
+      params.permit(:id, :name, :owner, :location_column, :location_type, :weight_column, :datafile)
     end
+
 end
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -25,40 +25,17 @@ class MapsController < ApplicationController
   # POST /maps.json
   def create
     @map = Map.new(map_params)
-
-    respond_to do |format|
-      if @map.save
-        format.html { redirect_to @map, notice: 'Map was successfully created.' }
-        format.json { render :show, status: :created, location: @map }
-      else
-        format.html { render :new }
-        format.json { render json: @map.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /maps/1
   # PATCH/PUT /maps/1.json
   def update
-    respond_to do |format|
-      if @map.update(map_params)
-        format.html { redirect_to @map, notice: 'Map was successfully updated.' }
-        format.json { render :show, status: :ok, location: @map }
-      else
-        format.html { render :edit }
-        format.json { render json: @map.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /maps/1
   # DELETE /maps/1.json
   def destroy
-    @map.destroy
-    respond_to do |format|
-      format.html { redirect_to maps_url, notice: 'Map was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
   end
 
   private
@@ -69,6 +46,6 @@ class MapsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def map_params
-      params[:map]
+      params.permit(:id, :name, :owner, :dataset, :display_variable, :filter_variable, :styling)
     end
 end
