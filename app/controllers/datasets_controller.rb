8126429f6c9c69bba985f6b979ff254c0c8df3c2
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-10-19 22:58:40 -0700

    Added code for generating dataset points, need to debug it

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,5 +1,5 @@
 class DatasetsController < ApplicationController
-  before_action :set_dataset, only: [:show, :edit, :update, :destroy]
+  before_action :set_dataset, only: [:show, :edit, :update, :destroy, :points]
 
   # GET /datasets
   # GET /datasets.json
@@ -24,13 +24,14 @@ class DatasetsController < ApplicationController
   # POST /datasets
   # POST /datasets.json
   def create
-
-    # Validate params
-    # Save file somewhere
-    # Read columns
-    # Create dataset creation params
-    # Create dataset
-    # Create columns
+    params = dataset_params
+    # TODO: Iteration 2
+    # TODO: Validate params
+    # TODO: Consume file (validate it, condense it, write it to file, read the columns)
+    
+    # TODO: Create dataset creation params
+    # TODO: Create dataset
+    # TODO: Create columns
     # Send responses
 
   end
@@ -42,6 +43,15 @@ class DatasetsController < ApplicationController
     # Delete the datafile
   end
 
+
+  def points
+    params = point_params
+    num_points = params[:num_points]
+    display_val = params[:display_val]
+    filter_val = params[:filter_val]
+    points = @datapoint.generate_points(1000)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_dataset
@@ -51,6 +61,10 @@ class DatasetsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def dataset_params
       params.permit(:id, :name, :owner, :location_column, :location_type, :weight_column, :datafile)
+    end
+
+    def point_params
+      params.permit(:id, :num_points, :display_val, :filter_val)
     end
 
 end
--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,3 +1,81 @@
+require 'csv'
+
 class Dataset < ActiveRecord::Base
   belongs_to :user
+
+    def consume_raw_file(filestream, filename)
+        # TODO: Process the file. Condense it. 
+        outpath = File.join(Rails.root, 'datasets', filename)
+        # TODO: Write the file here
+        # TODO: Return the filepath and the column names
+    end
+
+    def generate_points(num_points_wanted, display_val_name, filter_val_name)
+        location_column_name = self.location_column
+        weight_column_name = self.weight_column
+        filepath = self.filepath
+
+        by_location = self.merge_repeats(filepath, location_column_name, weight_column_name, display_val_name, filter_val_name)
+
+        num_condensed = 0
+        by_location.each do |location, point_dict|
+            num_condensed += point_dict.size
+        end
+
+        condense_factor = num_condensed / num_points_wanted
+
+        ans = condense_by_location(by_location, condense_factor)
+        return ans
+    end
+
+    def merge_repeats(filepath, location_column_name, weight_column_name, display_val_name, filter_val_name)
+        line_num = 0
+        by_location = Hash.new
+        CSV.foreach(filepath, :headers => true) do |row|
+            loc = row[location_column_name]
+
+            if not by_location.has_key?(loc)
+                by_location[loc] = Hash.new
+            end
+
+            display_val = row[display_val_name]
+            filter_val = row[filter_val_name] # TODO: or nil if we don't have a filter
+            weight = row[weight_column_name].to_i
+            key = [display_val, filter_val]
+
+            if not by_location[loc].has_key?(key)
+                by_location[loc][key] = 0
+            end
+
+            by_location[loc][key] += weight
+
+            if line_num % 10000 == 0
+                puts line_num
+            end
+            
+            line_num += 1
+        end
+        return by_location
+    end
+
+    def condense_by_location(by_location, condense_factor)
+        entry_probability = 1.0 / condense_factor
+        ans = Array.new
+        by_location.each do |location, point_dict|
+            point_dict.each do |key, weight|
+                display_val = key[0]
+                filter_val = key[1]
+                # TODO: figure out a better way to do this than just randomly
+                if rand() < entry_probability
+                    to_add = {"location" => location,
+                            "display_val" => display_val,
+                            "filter_val" => filter_val,
+                            "weight" => weight}
+                    ans.push(to_add)
+                end
+            end
+        end
+        return ans
+    end
+
 end
