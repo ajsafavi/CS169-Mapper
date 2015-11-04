ae19e15a307cc627c5e3ee41f1651732c9e39ca4
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-11-04 01:07:55 -0800

    Changed token auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   namespace :api do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-    end
+    mount_devise_token_auth_for 'User', at: 'auth'
   end
 
   get 'users/:id' => 'users#show'
