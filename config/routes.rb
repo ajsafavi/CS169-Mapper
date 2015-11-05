48808e71887bab745546dd59d65400831cab612c
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-11-04 18:23:09 -0800

    Added sign in and sign up links to the nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
 
   devise_for :users
   
-  namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth'
-  end
+  get 'users/:id' => 'users#show'
+
+  # namespace :api do
+  #   mount_devise_token_auth_for 'User', at: 'auth'
+  # end
 
   get 'users/auth_example' => 'users#auth_example'
-  get 'users/:id' => 'users#show'
+
+  
   root 'welcome#index'
   get 'welcome/index'
   
